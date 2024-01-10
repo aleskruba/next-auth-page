92e429a4ab30a83c17fb5c551b6e93e0febf9e5e
Aleš Kruba <aleskruba@seznam.cz>
2024-01-10 10:14:54 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
-
+// npm i prisma
+// npx prisma init --datasource-provider sqllite
+// npx prisma migrate dev --name first migration
+// npm i next-auth
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
